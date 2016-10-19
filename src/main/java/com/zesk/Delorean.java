502bd52f5e564e0224fc43f9797ae689cde493d3
Emmet Brown <emmet.brown@hill_valley.com>
2016-10-19 08:39:37 +0200

    addPlutonium method

--- src/main/java/com/zesk/Delorean.java
+++ src/main/java/com/zesk/Delorean.java
@@ -8,6 +8,7 @@ package com.zesk;
 public class Delorean
 {
     private int speed;
+    private boolean plutonium;
 
     /*
      * @param speed: The speed in m/s
@@ -23,6 +24,14 @@ public class Delorean
         return this.speed;
     }
 
+    public void insertPlutonium(){
+        this.plutonium = true;
+    }
+
+    public boolean hasPlutonium(){
+        return this.plutonium;
+    }
+
     public static void main( String[] args )
     {
         System.out.println("Doc construit une delorean");
@@ -31,7 +40,7 @@ public class Delorean
 
 
         System.out.println("Doc refait le plein de plutonium");
-        //delo.insert_plutonium();
+        delo.insertPlutonium();
 
         System.out.println("Doc programme un voyage dans le temps pour retourner en 1955");
         //delo.set_target_date("05/11/1955");
